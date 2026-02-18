2f92fed1ff74b30919949893f0e944d4a036cc47
ismoilrz <ismoil4766451155@gmail.com>
2026-02-18 13:37:13 +0300

    portfolio page add

---
+++ src/constansts/portfolioData.jsx
@@ -0,0 +1,74 @@
+/////////////// Portfolio img ///////////////
+import rendcar from "../assets/rendcar.jpg";
+import allCars from "../assets/allcars.jpg";
+import portfolio from "../assets/portfolio.jpg";
+import sl from "../assets/sl.jpg";
+import reality from "../assets/reality.jpg";
+
+////////////// Portfolio Url ///////////////
+const rendcarLink = "https://rendcar-seven.vercel.app";
+const allcarsLink = "https://all-cars.vercel.app";
+const portfolioLink = "https://portfolio-ismoil-rz.vercel.app";
+const slLink = "https://sl-sand.vercel.app";
+const realityLink = "https://reality-gaming.vercel.app";
+
+
+
+export const PortfolioData = [
+    {
+        id: 1,
+        path: rendcarLink,
+        pick: rendcar,
+        title: "Rendcar",
+    },
+    {
+        id: 2,
+        path: allcarsLink,
+        pick: allCars,
+        title: "All Cars",
+    },
+    {
+        id: 3,
+        path: portfolioLink,
+        pick: portfolio,
+        title: "Portfolio 1+",
+    },
+    {
+        id: 4,
+        path: slLink,
+        pick: sl,
+        title: "SL Basic",
+    },
+    {
+        id: 5,
+        path: realityLink,
+        pick: reality,
+        title: "Reality Gaming",
+    },
+    {
+        id: 6,
+        path: "#",
+        pick: "https://media.istockphoto.com/id/1413057319/ru/векторная/скоро-марка.jpg?s=612x612&w=0&k=20&c=yCkjzMH5_B7S5R2R2SLpq4KmhAedXpG-7kkw1QYs5Fg=",
+        title: "Coming Soon",
+    },
+    {
+        id: 7,
+        path: "#",
+        pick: "https://media.istockphoto.com/id/1413057319/ru/векторная/скоро-марка.jpg?s=612x612&w=0&k=20&c=yCkjzMH5_B7S5R2R2SLpq4KmhAedXpG-7kkw1QYs5Fg=",
+        title: "Coming Soon",
+    },
+    {
+        id: 8,
+        path: "#",
+        pick: "https://media.istockphoto.com/id/1413057319/ru/векторная/скоро-марка.jpg?s=612x612&w=0&k=20&c=yCkjzMH5_B7S5R2R2SLpq4KmhAedXpG-7kkw1QYs5Fg=",
+        title: "Coming Soon",
+    },
+    {
+        id: 9,
+        path: "#",
+        pick: "https://media.istockphoto.com/id/1413057319/ru/векторная/скоро-марка.jpg?s=612x612&w=0&k=20&c=yCkjzMH5_B7S5R2R2SLpq4KmhAedXpG-7kkw1QYs5Fg=",
+        title: "Coming Soon",
+    },
+]
+
+
--- src/pages/portfolio/portfolio.jsx
+++ src/pages/portfolio/portfolio.jsx
@@ -1,6 +1,21 @@
+import { PortfolioData } from "../../constansts/portfolioData";
+import "./portfolio.css"
+
 const Portfolio = () => {
     return <>
-        Portfolio
+        <section className="portfolio">
+            <h1 className="portfolioTitle">Portfolio</h1>
+                <div className="portfolioMain">
+                    {PortfolioData.map((item, i) => (
+                            <a key={i} href={item.path}>
+                                <div className="projectCart">
+                                    <img src={item.pick} alt={item.title} />
+                                </div>
+                                <h5>{item.title}</h5>
+                            </a>
+                    ))}  
+                </div>
+        </section>
     
     </>
 }
